9f7c5b68e5663da641ee8e4d29dde7514fc40aab
widyrii <widya985@gmail.com>
2016-02-12 15:32:49 +0700

    Create Menu

--- application/views/dashboard/menu.php
+++ application/views/dashboard/menu.php
@@ -31,115 +31,72 @@
                     <div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
 
                         <div class="menu_section">
-                            <h3>General</h3>
+                            <h3>Navigation</h3>
                             <ul class="nav side-menu">
-                                <li><a><i class="fa fa-home"></i> Home <span class="fa fa-chevron-down"></span></a>
+                                <li><a><i class="fa fa-dashboard"></i> Dasboard </a>
+                                    
+                                </li>
+                                <li><a><i class="fa fa-university"></i> Company <span class="fa fa-chevron-left"></span></a>
                                     <ul class="nav child_menu" style="display: none">
-                                        <li><a href="index.html">Dashboard</a>
-                                        </li>
-                                        <li><a href="index2.html">Dashboard2</a>
-                                        </li>
-                                        <li><a href="index3.html">Dashboard3</a>
-                                        </li>
+                                        <li><a href="form.html"><i class="fa fa-list"></i>All Data</a>
+                                        
                                     </ul>
                                 </li>
-                                <li><a><i class="fa fa-edit"></i> Forms <span class="fa fa-chevron-down"></span></a>
+                                <li><a><i class="fa fa-sitemap"></i> Management <span class="fa fa-chevron-left"></span></a>
                                     <ul class="nav child_menu" style="display: none">
-                                        <li><a href="form.html">General Form</a>
-                                        </li>
-                                        <li><a href="form_advanced.html">Advanced Components</a>
-                                        </li>
-                                        <li><a href="form_validation.html">Form Validation</a>
-                                        </li>
-                                        <li><a href="form_wizards.html">Form Wizard</a>
-                                        </li>
-                                        <li><a href="form_upload.html">Form Upload</a>
-                                        </li>
-                                        <li><a href="form_buttons.html">Form Buttons</a>
-                                        </li>
-                                    </ul>
-                                </li>
-                                <li><a><i class="fa fa-desktop"></i> UI Elements <span class="fa fa-chevron-down"></span></a>
+                                        <li><a><i class="fa fa-user"></i> Employe <span class="fa fa-chevron-left"></span></a>
                                     <ul class="nav child_menu" style="display: none">
-                                        <li><a href="general_elements.html">General Elements</a>
+                                        <li><a href="media_gallery.html"><i class="fa fa-bars"></i>All</a>
                                         </li>
-                                        <li><a href="media_gallery.html">Media Gallery</a>
-                                        </li>
-                                        <li><a href="typography.html">Typography</a>
-                                        </li>
-                                        <li><a href="icons.html">Icons</a>
-                                        </li>
-                                        <li><a href="glyphicons.html">Glyphicons</a>
-                                        </li>
-                                        <li><a href="widgets.html">Widgets</a>
-                                        </li>
-                                        <li><a href="invoice.html">Invoice</a>
-                                        </li>
-                                        <li><a href="inbox.html">Inbox</a>
-                                        </li>
-                                        <li><a href="calender.html">Calender</a>
-                                        </li>
-                                    </ul>
-                                </li>
-                                <li><a><i class="fa fa-table"></i> Tables <span class="fa fa-chevron-down"></span></a>
+                                        </ul>
+                                        <li><a><i class="fa fa-user"></i> New Employe <span class="fa fa-chevron-left"></span></a>
                                     <ul class="nav child_menu" style="display: none">
-                                        <li><a href="tables.html">Tables</a>
+                                        <li><a href="media_gallery.html"><i class="fa fa-bars"></i>All</a>
                                         </li>
-                                        <li><a href="tables_dynamic.html">Table Dynamic</a>
-                                        </li>
-                                    </ul>
-                                </li>
-                                <li><a><i class="fa fa-bar-chart-o"></i> Data Presentation <span class="fa fa-chevron-down"></span></a>
+                                        </ul>
+                                        <li><a><i class="fa fa-desktop"></i> Level <span class="fa fa-chevron-left"></span></a>
                                     <ul class="nav child_menu" style="display: none">
-                                        <li><a href="chartjs.html">Chart JS</a>
+                                        <li><a href="media_gallery.html"><i class="fa fa-bars"></i>All</a>
                                         </li>
-                                        <li><a href="chartjs2.html">Chart JS2</a>
-                                        </li>
-                                        <li><a href="morisjs.html">Moris JS</a>
-                                        </li>
-                                        <li><a href="echarts.html">ECharts </a>
-                                        </li>
-                                        <li><a href="other_charts.html">Other Charts </a>
-                                        </li>
-                                    </ul>
-                                </li>
-                            </ul>
-                        </div>
-                        <div class="menu_section">
-                            <h3>Live On</h3>
-                            <ul class="nav side-menu">
-                                <li><a><i class="fa fa-bug"></i> Additional Pages <span class="fa fa-chevron-down"></span></a>
+                                        </ul>
+                                        <li><a><i class="fa fa-desktop"></i> Division <span class="fa fa-chevron-left"></span></a>
                                     <ul class="nav child_menu" style="display: none">
-                                        <li><a href="e_commerce.html">E-commerce</a>
+                                        <li><a href="media_gallery.html"><i class="fa fa-bars"></i>All</a>
                                         </li>
-                                        <li><a href="projects.html">Projects</a>
-                                        </li>
-                                        <li><a href="project_detail.html">Project Detail</a>
-                                        </li>
-                                        <li><a href="contacts.html">Contacts</a>
-                                        </li>
-                                        <li><a href="profile.html">Profile</a>
-                                        </li>
-                                    </ul>
-                                </li>
-                                <li><a><i class="fa fa-windows"></i> Extras <span class="fa fa-chevron-down"></span></a>
+                                        </ul>
+                                        <li><a><i class="fa fa-money"></i> Salary <span class="fa fa-chevron-left"></span></a>
                                     <ul class="nav child_menu" style="display: none">
-                                        <li><a href="page_404.html">404 Error</a>
+                                        <li><a href="media_gallery.html"><i class="fa fa-bars"></i>All</a>
                                         </li>
-                                        <li><a href="page_500.html">500 Error</a>
-                                        </li>
-                                        <li><a href="plain_page.html">Plain Page</a>
-                                        </li>
-                                        <li><a href="login.html">Login Page</a>
-                                        </li>
-                                        <li><a href="pricing_tables.html">Pricing Tables</a>
-                                        </li>
+                                        </ul>
+                                    </li>
+                                </ul>
 
+                                <li><a><i class="fa fa-table"></i> Absent <span class="fa fa-chevron-left"></span></a>
+                                    <ul class="nav child_menu" style="display: none">
+                                        <li><a><i class="fa fa-bars"></i> All <span class="fa fa-chevron-left"></span></a>
+                                    <ul class="nav child_menu" style="display: none">
+                                        <li><a href="media_gallery.html"><i class="fa fa-plus"></i>Add</a>
+                                        </li>
+                                        </ul>
+                                        <li><a><i class="fa fa-desktop"></i> Free <span class="fa fa-chevron-left"></span></a>
+                                    <ul class="nav child_menu" style="display: none">
+                                        <li><a href="media_gallery.html"><i class="fa fa-bars"></i>All</a>
+                                        </li>
+                                        </ul>
+                                        <li><a><i class="fa fa-desktop"></i> Cuti <span class="fa fa-chevron-left"></span></a>
+                                    <ul class="nav child_menu" style="display: none">
+                                        <li><a href="media_gallery.html"><i class="fa fa-bars"></i>All</a>
+                                        </li>
+                                        </ul>
+                                        <li><a><i class="fa fa-desktop"></i> Overtime <span class="fa fa-chevron-left"></span></a>
+                                    <ul class="nav child_menu" style="display: none">
+                                        <li><a href="media_gallery.html"><i class="fa fa-bars"></i>All</a>
+                                        </li>
+                                        </ul>
                                     </ul>
                                 </li>
-                                <li><a><i class="fa fa-laptop"></i> Landing Page <span class="label label-success pull-right">Coming Soon</span></a>
-                                </li>
-                            </ul>
+                                
                         </div>
 
                     </div>
@@ -201,7 +158,7 @@
                                     <i class="fa fa-envelope-o"></i>
                                     <span class="badge bg-green">6</span>
                                 </a>
-                                <ul id="menu1" class="dropdown-menu list-unstyled msg_list animated fadeInDown" role="menu">
+                                <ul id="menu1" class="dropdown-menu bars-unstyled msg_bars animated fadeInDown" role="menu">
                                     <li>
                                         <a>
                                             <span class="image">
@@ -625,7 +582,7 @@
                             </div>
                             <div class="x_content">
                                 <div class="dashboard-widget-content">
-                                    <ul class="quick-list">
+                                    <ul class="quick-bars">
                                         <li><i class="fa fa-calendar-o"></i><a href="#">Settings</a>
                                         </li>
                                         <li><i class="fa fa-bars"></i><a href="#">Subscription</a>
@@ -682,7 +639,7 @@
                             <div class="x_content">
                                 <div class="dashboard-widget-content">
 
-                                    <ul class="list-unstyled timeline widget">
+                                    <ul class="bars-unstyled timeline widget">
                                         <li>
                                             <div class="block">
                                                 <div class="block_content">
@@ -778,7 +735,7 @@
                                             <div class="col-md-4 hidden-small">
                                                 <h2 class="line_30">125.7k Views from 60 countries</h2>
 
-                                                <table class="countries_list">
+                                                <table class="countries_bars">
                                                     <tbody>
                                                         <tr>
                                                             <td>United States</td>
@@ -813,11 +770,11 @@
                         <div class="row">
 
 
-                            <!-- Start to do list -->
+                            <!-- Start to do bars -->
                             <div class="col-md-6 col-sm-6 col-xs-12">
                                 <div class="x_panel">
                                     <div class="x_title">
-                                        <h2>To Do List <small>Sample tasks</small></h2>
+                                        <h2>To Do bars <small>Sample tasks</small></h2>
                                         <ul class="nav navbar-right panel_toolbox">
                                             <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
                                             </li>
@@ -880,7 +837,7 @@
                                     </div>
                                 </div>
                             </div>
-                            <!-- End to do list -->
+                            <!-- End to do bars -->
 
 
                             <!-- start of weather widget -->
