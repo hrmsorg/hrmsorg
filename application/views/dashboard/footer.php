41d0248fb8034a8e73e7e27486e9fb7e0fed15c5
sopiehalimah <sopiehalimah@gmail.com>
2016-02-15 13:46:41 +0700

    "folder"

--- application/views/dashboard/footer.php
+++ application/views/dashboard/footer.php
@@ -1,7 +1,7 @@
                 <footer>
                     <div class="">
-                        <p class="pull-right">Gentelella Alela! a Bootstrap 3 template by <a>Kimlabs</a>. |
-                            <span class="lead"> <i class="fa fa-paw"></i> Gentelella Alela!</span>
+                        <p class="pull-right">PG<a>hrms</a>. |
+                            <span class="lead"> <i class="fa fa-paw"></i> PG</span>
                         </p>
                     </div>
                     <div class="clearfix"></div>
