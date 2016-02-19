fd31859dfa8327491ff2ee704e6ceeb77d5ffc1d
sopiehalimah <sopiehalimah@gmail.com>
2016-02-19 09:51:58 +0700

    footer,header

---
+++ application/controllers/Absent.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+* 
+*/
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Absent extends CI_Controller
+{
+
+		public function cuti()
+
+	{
+
+		$this->load->view('absent/cuti');
+	
+	}
+
+}
