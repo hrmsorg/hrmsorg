a2d53250056433ba7a61bf8e3b28ba3ae6bbf9d6
amandaazallia <mandazal123@gmail.com>
2016-03-01 10:03:32 +0700

    revisi tampilan divisi

--- application/views/management/division/all.php
+++ application/views/management/division/all.php
@@ -29,21 +29,11 @@
                             <!-- form date pickers -->
                             <div class="x_panel" style="">
                                 <div class="x_title">
-                                    <h2>Form Division<small> Add new</small></h2>
+                                    <h2>Form Division</h2>
                                     <ul class="nav navbar-right panel_toolbox">
-                                        <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
+                                        <li><a class="collapse-link"><i class="fa fa-chevron-up"><small> Add new</small></i></a>
                                         </li>
-                                        <li class="dropdown">
-                                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-wrench"></i></a>
-                                            <ul class="dropdown-menu" role="menu">
-                                                <li><a href="#">Settings 1</a>
-                                                </li>
-                                                <li><a href="#">Settings 2</a>
-                                                </li>
-                                            </ul>
-                                        </li>
-                                        <li><a class="close-link"><i class="fa fa-close"></i></a>
-                                        </li>
+
                                     </ul>
                                     <div class="clearfix"></div>
                                 </div>
